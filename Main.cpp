da687290fb91037c4eb26be54f2d6d61fd9272f3
Georgi Yordanov <georgimyordanov07@gmail.com>
2025-12-18 21:19:13 +0200

    Camera AND movemet!!!

--- Main.cpp
+++ Main.cpp
@@ -9,6 +9,7 @@
 #include <glm/glm.hpp>
 #include <glm/gtc/matrix_transform.hpp>
 #include <glm/gtc/type_ptr.hpp>
+#include "Camera.h"
 
 // glfw: whenever the window size changes this callback function executes
 void framebuffer_size_callback(GLFWwindow* window, int width, int height);
@@ -16,9 +17,21 @@ void framebuffer_size_callback(GLFWwindow* window, int width, int height);
 // On escape close the window
 void processInput(GLFWwindow* window);
 
+void mouse_callback(GLFWwindow* window, double xpos, double ypos);
+
+void scroll_callback(GLFWwindow* window, double xoffset, double yoffset);
+
 // settings
 const unsigned int SCR_WIDTH = 1280;
 const unsigned int SCR_HEIGHT = 720;
+
+float deltaTime = 0.0f;
+float lastFrame = 0.0f;
+
+Camera camera(glm::vec3(0.0f, 0.0f, 3.0f));
+bool firstMouse = true;
+float lastX = 800.0f / 2.0;
+float lastY = 600.0 / 2.0;
 
 int main()
 {
@@ -41,7 +54,10 @@ int main()
     }
 
     glfwMakeContextCurrent(window);
-
+    glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
+    glfwSetCursorPosCallback(window, mouse_callback);
+    glfwSetScrollCallback(window, scroll_callback);
+    glfwSetInputMode(window, GLFW_CURSOR, GLFW_CURSOR_DISABLED);
     // Disable VSync (uncap FPS). 0 = off, 1 = on, 2 = every other v-blank (if supported)
     glfwSwapInterval(0);
 
@@ -52,12 +68,9 @@ int main()
         return -1;
     }
 
-    // configure global opengl state
-    // -----------------------------
     glEnable(GL_DEPTH_TEST);
-
     glViewport(0, 0, SCR_WIDTH, SCR_HEIGHT);
-    glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
+    
 
     Shader rectangleShader("vertex.glsl", "rectangle.frag");
 
@@ -186,6 +199,7 @@ int main()
     rectangleShader.use(); // don't forget to activate the shader before setting uniforms!  
     rectangleShader.setInt("texture1", 0);
     rectangleShader.setInt("texture2", 1);
+
 	// FPS measurement variables
     double previousTime = glfwGetTime();
     int frameCount = 0;
@@ -210,22 +224,14 @@ int main()
 
         rectangleShader.use();
 
-        rectangleShader.setVec4("funnyRectColor", cos(glfwGetTime()), sin(glfwGetTime()), sin(glfwGetTime()), 1.0f);
-
-        // create transformations
-        glm::mat4 model = glm::mat4(1.0f); // make sure to initialize matrix to identity matrix first
-        glm::mat4 view = glm::mat4(1.0f);
-        glm::mat4 projection = glm::mat4(1.0f);
-        view = glm::translate(view, glm::vec3(0.0f, 0.0f, -3.0f));
-        projection = glm::perspective(glm::radians(45.0f), (float)SCR_WIDTH / (float)SCR_HEIGHT, 0.1f, 100.0f);
-        // retrieve the matrix uniform locations
-        unsigned int modelLoc = glGetUniformLocation(rectangleShader.ID, "model");
-        unsigned int viewLoc = glGetUniformLocation(rectangleShader.ID, "view");
-        // pass them to the shaders (3 different ways)
-        glUniformMatrix4fv(modelLoc, 1, GL_FALSE, glm::value_ptr(model));
-        glUniformMatrix4fv(viewLoc, 1, GL_FALSE, &view[0][0]);
-        // note: currently we set the projection matrix each frame, but since the projection matrix rarely changes it's often best practice to set it outside the main loop only once.
+        // Camera stuff
+        glm::mat4 projection = glm::perspective(glm::radians(camera.Zoom), (float)SCR_WIDTH / (float)SCR_HEIGHT, 0.1f, 100.0f);
         rectangleShader.setMat4("projection", projection);
+
+        glm::mat4 view = camera.GetViewMatrix();
+        
+        rectangleShader.setMat4("view", view);
+        rectangleShader.setVec4("funnyRectColor", cos(glfwGetTime()), sin(glfwGetTime()), sin(glfwGetTime()), 1.0f);
 
         // render box
         glBindVertexArray(VAOs[0]);
@@ -234,7 +240,12 @@ int main()
             glm::mat4 model = glm::mat4(1.0f);
             model = glm::translate(model, cubePositions[i]);
             float angle = 20.0f * i;
-            model = glm::rotate(model, (glm::radians(angle) + 0.05f) * (float)glfwGetTime(), glm::vec3(1.0f, 0.3f, 0.5f));
+            
+            if (i % 2 == 0)
+                model = glm::rotate(model, (glm::radians(angle) + 0.05f) * (float)glfwGetTime(), glm::vec3(1.0f, 0.3f, 0.5f));
+            else
+                model = glm::rotate(model, glm::radians(angle), glm::vec3(1.0f, 0.3f, 0.5f));
+
             rectangleShader.setMat4("model", model);
 
             glDrawArrays(GL_TRIANGLES, 0, 36);
@@ -278,4 +289,44 @@ void framebuffer_size_callback(GLFWwindow* window, int width, int height)
 void processInput(GLFWwindow* window)
 {
     if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS) glfwSetWindowShouldClose(window, true);
+
+    float currentFrame = glfwGetTime();
+    deltaTime = currentFrame - lastFrame;
+    lastFrame = currentFrame;
+    const float cameraSpeed = 2.5f * deltaTime;
+
+    if (glfwGetKey(window, GLFW_KEY_W) == GLFW_PRESS)
+        camera.ProcessKeyboard(FORWARD, deltaTime);
+    if (glfwGetKey(window, GLFW_KEY_S) == GLFW_PRESS)
+        camera.ProcessKeyboard(BACKWARD, deltaTime);
+    if (glfwGetKey(window, GLFW_KEY_A) == GLFW_PRESS)
+        camera.ProcessKeyboard(LEFT, deltaTime);
+    if (glfwGetKey(window, GLFW_KEY_D) == GLFW_PRESS)
+        camera.ProcessKeyboard(RIGHT, deltaTime);
+}
+
+void mouse_callback(GLFWwindow* window, double xpos, double ypos)
+{
+    if (firstMouse)
+    {
+        lastX = xpos;
+        lastY = ypos;
+        firstMouse = false;
+    }
+
+    float xoffset = xpos - lastX;
+    float yoffset = lastY - ypos;
+    lastX = xpos;
+    lastY = ypos;
+
+    float sensitivity = 0.1f;
+    xoffset *= sensitivity;
+    yoffset *= sensitivity;
+
+    camera.ProcessMouseMovement(xoffset, yoffset);
+}
+
+void scroll_callback(GLFWwindow* window, double xoffset, double yoffset)
+{
+    camera.ProcessMouseScroll(static_cast<float>(yoffset));
 }
